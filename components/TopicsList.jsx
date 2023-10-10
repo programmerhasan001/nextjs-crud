5946036c5a19a2c87dd7a0a6e13061f6f406f504
Hasan <programmermdhasan@gmail.com>
2023-10-10 11:37:38 +0600

    change data fetching

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,21 +2,49 @@ import Link from 'next/link';
 import { BiEdit } from 'react-icons/bi';
 import RemoveBtn from './RemoveBtn';
 
-export default function TopicsList() {
+
+const getTopics = async () => {
+    try {
+        const res = await fetch('http://localhost:3000/api/topics')
+        if (!res.ok) {
+            throw new Error("Failed to fetch Data");
+        }
+
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default async function TopicsList() {
+
+    const { topics } = await getTopics();
+
     return (
-        <div className='p-4 border border-slate-300 my-3 flex items-start justify-between gap-5'>
-            <div>
-                <h2 className='font-bold text-2xl'>Topic Title</h2>
-                <p>Topic Description</p>
-            </div>
-            <div className='flex gap-x-2'>
-                <Link href={'/edit-topic/123'}>
-                    <BiEdit size={24} />
-                </Link>
-                <Link href={'/'}>
-                    <RemoveBtn />
-                </Link>
-            </div>
-        </div>
+        <>
+            {
+                topics.length > 0 && topics.map(topic => {
+                    const { title, description, _id } = topic;
+                    console.log(topic)
+                    return (
+                        <div className='p-4 border border-slate-300 my-3 flex items-start justify-between gap-5'>
+                            <div>
+                                <h2 className='font-bold text-2xl'>{title}</h2>
+                                <p>{description}</p>
+                            </div>
+                            <div className='flex gap-x-2'>
+                                <Link href={`/edit-topic/${_id}`}>
+                                    <BiEdit size={24} />
+                                </Link>
+                                <Link href={`/${_id}`}>
+                                    <RemoveBtn />
+                                </Link>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 }
